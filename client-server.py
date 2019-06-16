a5c72b0bd94eaa91d1b670998ea4405366ea3f06
henrychia12 <henry_chia21@hotmail.co.uk>
2019-06-16 18:18:05 +0100

    public key being exchanged

--- client-server.py
+++ client-server.py
@@ -1,5 +1,12 @@
 import socket
+import os
+from cryptography.fernet import Fernet
+
 s = socket.socket()
+
+path = os.path.join(os.path.expanduser("~"), ".ssh", "id_rsa.pub")
+public_key_file = open(path, "rb")
+public_key = public_key_file.read()
 
 hostname = "henrylaptop"
 port = 8000
@@ -8,5 +15,6 @@ s.connect((hostname,port))
 
 while True:
 	x = raw_input("Enter message: ")
-	s.send(x.encode())
+	s.send(x.encode() + "\n")
+	s.send(public_key.encode())
 	
